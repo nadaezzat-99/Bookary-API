e6922120bc2de08eeee7b1a0b44adda6e83ec343
nadaezzat-99 <nadaezzat507@gmail.com>
2023-04-16 05:24:40 +0200

    edit auth

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,29 +1,27 @@
-import { Request, Response, NextFunction } from "express";
-import { Role } from "../DB/schemaInterfaces";
-import { AppError } from "../lib/index";
+import { Request, Response, NextFunction } from 'express';
+import { Role } from '../DB/schemaInterfaces';
+import { AppError } from '../lib/index';
 
-const jwt = require("jsonwebtoken");
-const Users = require("../DB/models/user");
+const jwt = require('jsonwebtoken');
+const Users = require('../DB/models/user');
+
 
 const verifyToken = async (bearerToken: string) => {
-  bearerToken = bearerToken.split(" ")[1];
-  if (!bearerToken) return new AppError("Sign in again", 401);
+  bearerToken = bearerToken.split(' ')[1];
+  if(!bearerToken) return new AppError('Sign in again',401); 
   const decoded = jwt.verify(bearerToken, process.env.TOKEN_KEY);
-  const user = await Users.findById(decoded.userId);
-  if (!user) return new AppError("un-authenticated", 401);
+  const user = await Users.findById( decoded.userId);
+  if(!user) return new AppError('un-authenticated',401); 
   return user;
 };
 
-const checkAuth = async (bearerToken: string | undefined, role: Role) => {
-  if (!bearerToken) throw new Error("Un-Authenticated");
-  const result = await verifyToken(bearerToken);
-  if (result.role !== role) throw new AppError("Unauthorized-User", 403);
-  return result;
-};
-
 const userAuth = async (req: Request, res: Response, next: NextFunction) => {
+  let bearerToken = req.headers.authorization;
   try {
-    req.user = checkAuth(req.headers.authorization, Role.USER);
+    if (!bearerToken) throw new Error('Un-Authenticated');
+    const result = await verifyToken(bearerToken);
+    if (result.role !== Role.USER) throw new AppError('Unauthorized-User',403);
+    req.user = result
     next();
   } catch (err) {
     next(err);
@@ -31,8 +29,12 @@ const userAuth = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const adminAuth = async (req: Request, res: Response, next: NextFunction) => {
+  let bearerToken = req.headers.authorization;  
   try {
-    req.user = checkAuth(req.headers.authorization, Role.ADMIN);
+    if (!bearerToken) throw new Error('Unauthenticated-User');
+    const result = await verifyToken(bearerToken);
+    if (result.role !== Role.ADMIN) throw new AppError('Unauthorized-User',403);
+    req.user = result;
     next();
   } catch (err) {
     next(err);
